922e751823822c9f3f3d1430403761aaa41254c8
jkb7 <Jacob202@protonmail.com>
2023-12-20 09:31:33 +0100

    Added subjects.json generator

---
+++ src/subjectsGenerator.py
@@ -0,0 +1,38 @@
+import json, itertools
+
+data = {"subjects": {}}
+
+while True:
+    subject = input(f"\n{'':-^30}\nEnter a subject: ")
+    if subject == "":
+        print("Finished generating subjects")
+        break
+    else:
+        data["subjects"][subject] = {"groups": []}
+
+    for index in itertools.count():
+        print(f"\nCreating a group #{index + 1}")
+        
+        limit = input("Students limit: ")
+        if limit == "":
+            print("Subject creation finished")
+            break
+    
+        day = input("Day: ")
+        timeStart = input("Time start: ")
+        timeEnd = input("Time end: ")
+        frequency = input("Frequency: ")
+        collisions = input("Collisions: ")
+        print(collisions.split(", "))
+
+        data["subjects"][subject]["groups"].append({
+            "limit": int(limit),
+            "day": day,
+            "timeStart": timeStart,
+            "timeEnd": timeEnd,
+            "frequency": int(frequency),
+            "collisions": collisions.split(", ")
+        })
+
+with open("subjects.json", "w") as file:
+    json.dump(data, file, indent=2)
