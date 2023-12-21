17f2b400bd2f6ca64153143aa0354794e49f302f
jkb7 <Jacob202@protonmail.com>
2023-12-21 21:15:55 +0100

    Added file structure

---
+++ xlparser/scripts/subjectsGenerator.py
@@ -0,0 +1,38 @@
+import json, itertools, os
+
+data = {"subjects": {}}
+
+while True:
+    subject = input(f"\n{'':-^30}\nEnter a subject: ")
+    if subject == "":
+        print("Finished generating subjects")
+        break
+    else:
+        data["subjects"][subject] = {"groups": []}
+
+    for index in itertools.count():
+        print(f"\nCreating a group #{index + 1}")
+        
+        limit = input("Students limit: ")
+        if limit == "":
+            print("Subject creation finished")
+            break
+    
+        day = input("Day: ")
+        timeStart = input("Time start: ")
+        timeEnd = input("Time end: ")
+        frequency = input("Frequency: ")
+        collisions = input("Collisions: ")
+
+        data["subjects"][subject]["groups"].append({
+            "limit": int(limit),
+            "day": day,
+            "timeStart": timeStart,
+            "timeEnd": timeEnd,
+            "frequency": int(frequency),
+            "collisions": list(map(str.strip, collisions.split(","))) if collisions != "" else []
+        })
+
+outputPath = os.path.join(os.path.dirname(__file__), "..", "data/subjects.json")
+with open(outputPath, "w") as file:
+    json.dump(data, file, indent=2)
